b4d815bd970d4ec4261eaef8b1d2f8f82799a34e
Bajurov <66136599+Bajurov@users.noreply.github.com>
2020-05-31 08:33:36 +0300

    Add files via upload

---
+++ ChildForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ITMO.Group124.Bajurov.Lab1.Task4
+{
+    public partial class ChildForm : ITMO.Group124.Bajurov.Lab1.Task4.ParentForm
+    {
+        public ChildForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ToggleMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ToggleMenuItem.Checked)
+            {
+                ToggleMenuItem.Checked = false;
+                ChildTextBox.ForeColor = System.Drawing.Color.Black;
+            }
+            else
+            {
+                ToggleMenuItem.Checked = true;
+                ChildTextBox.ForeColor = System.Drawing.Color.Blue;
+            }
+        }
+    }
+}
